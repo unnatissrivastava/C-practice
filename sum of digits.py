a502d5e534c6d7298c07aade4246aba3791e69c9
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-18 16:01:33 +0530

    Update sum of digits.py

--- sum of digits.py
+++ sum of digits.py
@@ -1,1 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, total_sum, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    total_sum = 0;
+
+    while (number > 0)
+    {
+        digit = number % 10;
+        total_sum = number + digit;
+        number = number / 10;
+        
+    }
+
+    printf("sum of digits is: %d", total_sum);
+
+    return 0;
+}
 
