52ab4ca444fe6b043fb46704a76ec8ac4d0f1c6f
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-18 16:40:54 +0530

    Update number of 5s.c\

--- number of 5s.c
+++ number of 5s.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int n, temp, digit, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (temp != 0) {
+        digit = temp % 10;
+
+        if (digit == 5)
+            count++;
+
+        temp = temp / 10;
+    }
+
+    printf("Number of 5s = %d", count);
+
+    return 0;
+}
