a596f945a9cda8adf94118e58cec7403ca094920
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-18 16:19:15 +0530

    Update harshad number.c

--- harshad number.c
+++ harshad number.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main() {
+    int n, temp, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while (temp > 0) {
+        digit = temp % 10;
+        sum = sum + digit;
+        temp = temp / 10;
+    }
+
+    if (n % sum == 0)
+        printf("%d is a Harshad number", n);
+    else
+        printf("%d is not a Harshad number", n);
+
+    return 0;
+}
