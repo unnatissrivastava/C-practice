0dbb13585d080293b9e1528b20d92847e6236410
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-18 16:05:54 +0530

    Update product of digits.c

--- product of digits.c
+++ product of digits.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+    int number, total_sum, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    total_sum = 1;
+
+    while (number > 0)
+    {
+        digit = number % 10;
+        total_sum = number * digit;
+        number = number / 10;
+        
+    }
+
+    printf("product of digits is: %d", total_sum);
+
+    return 0;
+}
