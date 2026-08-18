a402ea1bc3dd8d6495d9dfd1046fd38c19f86e5a
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-18 16:18:02 +0530

    Update num of digits.c

--- num of digits.c
+++ num of digits.c
@@ -1,1 +1,20 @@
 
+
+#include <stdio.h>
+
+int main()
+{
+    int num,count;
+     printf("enter a number: ");
+    scanf("%d", &num);
+
+    count = 0;
+    while (num>0){
+        count +=1;   
+        num = num/10;}
+
+    printf("number of digits are:%d " ,count);
+
+
+    return 0;
+}
