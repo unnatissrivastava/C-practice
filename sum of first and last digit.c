7e6507919bbb50fe7b4a881b8538d62ea18210e0
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-18 16:06:18 +0530

    Update sum of first and last digit.c

--- sum of first and last digit.c
+++ sum of first and last digit.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+    int number, first_digit, last_digit, sum;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    last_digit = number % 10;
+
+    first_digit = number;
+    while (first_digit >= 10)
+    {
+        first_digit = first_digit / 10;
+    }
+
+    sum = first_digit + last_digit;
+
+    printf("Sum of first and last digit: %d", sum);
+
+    return 0;
+}
