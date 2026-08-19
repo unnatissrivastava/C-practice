c8d44e648d5848caadb0d11a7f6e3e687e731786
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-19 12:15:27 +0530

    Update traversing.c

--- traversing.c
+++ traversing.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main()
+{
+    int a[2][3] = {
+        {10, 20, 30},
+        {40, 50, 60}
+    };
+
+    int i, j;
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
