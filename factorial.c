9b92fae6b48969cf739311465cfdc21d81342d0d
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-19 11:44:53 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+    int fact, n, i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    fact = 1;
+
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    printf("Factorial is: %d", fact);
+
+    return 0;
+}
