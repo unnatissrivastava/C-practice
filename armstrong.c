eab9acc73ed1fab048dd9b9bdf9294a3f779d733
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-20 00:10:31 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n, digit=0, prod=0 , temp;
+    printf("enter a number: ");
+    scanf("%d", &n);
+    temp = n;
+    while (temp>0){
+        temp /= 10;
+        digit++;
+    }
+    temp = n;
+    
+    while (temp!=0){
+        digit = temp%10;
+        prod += digit*digit*digit;
+        temp /= 10;
+    }
+    if (prod == n) printf("armstrong number it is");
+    else printf("not armstrong");
+    return 0;
 
+}
