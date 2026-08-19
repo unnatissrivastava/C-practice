0cc8ee3eb34fad7a32bbefa9309b0ddba5140104
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-19 12:22:46 +0530

    Update finding even odd in 2d arr.c

--- finding even odd in 2d arr.c
+++ finding even odd in 2d arr.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main()
+{
+    int a[5], i;
+
+    printf("Enter 5 numbers: ");
+
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Even numbers: ");
+    for(i = 0; i < 5; i++)
+    {
+        if(a[i] % 2 == 0)
+            printf("%d ", a[i]);
+    }
+
+    printf("\nOdd numbers: ");
+    for(i = 0; i < 5; i++)
+    {
+        if(a[i] % 2 != 0)
+            printf("%d ", a[i]);
+    }
+
+    return 0;
+}
