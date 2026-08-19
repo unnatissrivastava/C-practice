a449084a77ec264fd8c697952be8a52897cea7f4
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-19 12:20:44 +0530

    Update finding element in 2d array.c

--- finding element in 2d array.c
+++ finding element in 2d array.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int main()
+{
+    int a[2][3] = {{10, 20, 30}, {40, 50, 60}};
+    int n, i, j, flag = 0;
+
+    printf("Enter number to search: ");
+    scanf("%d", &n);
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            if(a[i][j] == n)
+            {
+                flag = 1;
+                break;
+            }
+        }
+    }
+
+    if(flag == 1)
+        printf("Number is present");
+    else
+        printf("Number is not present");
+
+    return 0;
+}
