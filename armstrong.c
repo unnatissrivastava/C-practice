8b38988a331160fdb903b025c5f414452c48c7b4
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-19 11:46:42 +0530

    Update armstrong.c

--- armstrong.c
+++ armstrong.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
 
+int main()
+{
+    int n, digit, temp, digits, sum;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+    digits = 0;
+    sum = 0;
+
+    
+    while(temp > 0)
+    {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = n;
+
+   
+    while(temp > 0)
+    {
+        digit = temp % 10;
+        sum += digit * digit * digit;
+        temp /= 10;
+    }
+
+    if(sum == n)
+    {
+        printf("%d is an Armstrong number", n);
+    }
+    else
+    {
+        printf("%d is not an Armstrong number", n);
+    }
+
+    return 0;
+}
