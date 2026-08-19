cd4772626469498396cfd5239af957e0676ae525
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-19 12:00:56 +0530

    Update ascending-descending.c

--- ascending-descending.c
+++ ascending-descending.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main()
+{
+    int n, d1, d2, flag = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while(n > 0)
+    {
+        d1 = n % 10;
+        n = n / 10;
+        d2 = n % 10;
+
+        if(d1 > d2)
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if(flag == 1)
+    {
+        printf("Ascending");
+    }
+    else
+    {
+        printf("Not Ascending");
+    }
+
+    return 0;
+}
