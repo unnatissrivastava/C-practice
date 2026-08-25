6b0dd943dac68257ee2a928edecf35b970679cfb
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-25 16:26:20 +0530

    Update odd even malloc.c

--- odd even malloc.c
+++ odd even malloc.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, even = 0, odd = 0;
+    int *arr = (int*)malloc(n*sizeof(int));
+    for (int i = 0; i<n;i++){
 
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i<n; i++){
+        if (arr[i]%2 == 0){
+            even++;
+        }
+        else{
+            odd++;
+        }
+    }
+    printf("%d%d", even, odd);
+        return 0;
+       
+}
