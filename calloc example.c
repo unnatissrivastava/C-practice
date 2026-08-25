aa92db1df8c07f6793d8d9907f5f694d96b9440a
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-25 16:41:20 +0530

    Update calloc example.c

--- calloc example.c
+++ calloc example.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, even = 0, odd = 0;
+    int *arr = (int*)calloc(n, sizeof(int));
+    for (int i = 0; i<n;i++){
 
+        printf("%d", &arr[i]);
+    }
+    
+        return 0;
+       
+}
