488b02a6cf975cc4cbe895d6ae356f0ac7580a2a
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-25 15:40:17 +0530

    Update malloc.c

--- malloc.c
+++ malloc.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int *ptr =(int*) malloc(n*sizeof(int));
+    for (int i=0; i<n; i++){
+        scanf("%d", &ptr[i]);
+    }
+}
