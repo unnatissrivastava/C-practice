4ef5fee63ad96ceabf880dd3ff26f2a5ac0a96c6
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-25 16:10:41 +0530

    Update reversing array.c

--- reversing array.c
+++ reversing array.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int i = 0, j = n-1;
+    while(i<j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j]= temp;
+        i++;
+        j--;
+    }
 
+    return 0;
+       
+}
