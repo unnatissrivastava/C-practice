cf0985cb12ec05e60024628d45400ee2221bb84c
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-08-25 16:02:35 +0530

    Update malloc example 2.c

--- malloc example 2.c
+++ malloc example 2.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    int *arr = malloc(n*sizeof(int));
+    for (int i = 0; i<n ; i++){
+        scanf("%d", &arr[i]);
+        int max = arr[0];
+        for(int i=1; i<n;i++){
+            if(arr[i]>max);
+            max = arr[i];
+        }
+    }
+    return 0;
+       
+}
