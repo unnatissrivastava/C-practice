d6e007dfde32d4a84b66a49b334ef2cac9226245
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-08 11:58:29 +0530

    Update while-loop.c

--- while-loop.c
+++ while-loop.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main()
+{
+    int count,n;
+    float x,y;
+    printf("enter the values of x and n: ");
+    scanf("%f %d",&x, &n);
+    y = 1.0;
+    count = 1;
+    //loop begins
+    while(count <= n){
+        y = y*x;
+        count++;
+    }
+    printf("x = %f; n = %d; x to power n = %f n", x, n, y);
+    
+    return 0;
+    
+}
