e66bca83b0e141a044ea3a37ff5e6da99e8b4de4
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-08 16:10:26 +0530

    Update adding node.c

--- adding node.c
+++ adding node.c
@@ -4,3 +4,9 @@ struct Node{
     int data;
     struct Node* next;
 };
+struct Node* createNode(int val){
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode -> data = val;
+    newNode -> next = NULL;
+    return newNode;
+}
