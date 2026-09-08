a8999a550875ab74acd3c8d7c55ed051617eb2c7
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-08 16:22:08 +0530

    Update subtracting nodes.c

--- subtracting nodes.c
+++ subtracting nodes.c
@@ -5,25 +5,19 @@ struct Node {
     struct Node *next;
 };
 int main() {
-
     struct Node *head = malloc(sizeof(struct Node));
     struct Node *second = malloc(sizeof(struct Node));
     struct Node *third = malloc(sizeof(struct Node));
-
     // Add data
     head->data = 10;
     second->data = 20;
     third->data = 30;
-
     // Connect nodes
     head->next = second;
     second->next = third;
     third->next = NULL;
-
     // Subtraction
     int result = head->data - second->data - third->data;
-
     printf("Subtraction = %d", result);
-
     return 0;
 }
