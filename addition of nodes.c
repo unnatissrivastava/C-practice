0ad39a1062ce5eb7595a3a5ca9f54be7d32391ea
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-08 16:17:54 +0530

    Update addition of nodes.c

--- addition of nodes.c
+++ addition of nodes.c
@@ -6,7 +6,6 @@ struct Node {
 };
 int main() {
 
-    // Create 
     struct Node *head = malloc(sizeof(struct Node));
     struct Node *second = malloc(sizeof(struct Node));
     struct Node *third = malloc(sizeof(struct Node));
