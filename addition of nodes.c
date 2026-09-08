8af915f1d79a25fd3c114f6b3bc76fe22d80616d
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-08 16:19:32 +0530

    Update addition of nodes.c

--- addition of nodes.c
+++ addition of nodes.c
@@ -5,33 +5,26 @@ struct Node {
     struct Node *next;
 };
 int main() {
-
     struct Node *head = malloc(sizeof(struct Node));
     struct Node *second = malloc(sizeof(struct Node));
     struct Node *third = malloc(sizeof(struct Node));
     struct Node *fourth = malloc(sizeof(struct Node));
     struct Node *fifth = malloc(sizeof(struct Node));
-
     head->data = 10;
     second->data = 20;
     third->data = 30;
     fourth->data = 40;
     fifth->data = 50;
-
     head->next = second;
     second->next = third;
     third->next = fourth;
     fourth->next = fifth;
     fifth->next = NULL;
-
     struct Node *temp = head;
-
     while (temp != NULL) {
         printf("%d -> ", temp->data);
         temp = temp->next;
     }
-
     printf("NULL");
-
     return 0;
 }
