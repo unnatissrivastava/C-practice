1e9b1de3a5455f0cf4da0739f942faeb1e195d67
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-22 21:01:04 +0530

    Update queue-linked-list.c

--- queue-linked-list.c
+++ queue-linked-list.c
@@ -1,1 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;          
+    struct Node* next;
+};
+
+struct Node* front = NULL;
+struct Node* rear = NULL;
+
+struct Node* createNode(int val) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;  
+    newNode->next = NULL;
+    return newNode;
+}
+
+void enqueue(int val) {
+    struct Node* newNode = createNode(val);
+    if (front == NULL && rear == NULL) {
+        front = rear = newNode;
+    } else {
+        rear->next = newNode;
+        rear = newNode;
+    }
+    printf("Enqueued: %d\n", val);
+}
+
+void dequeue() {
+    if (front == NULL) {
+        printf("Underflow condition!!\n");
+        return;
+    }
+    
+    printf("Dequeued: %d\n", front->data);
+    struct Node* temp = front; 
+    front = front->next;
+    
+    if (front == NULL) {
+        rear = NULL;
+    }
+    
+    free(temp);
+}
+
+void display() {   
+    if (front == NULL) {
+        printf("Queue is empty\n");
+        return;
+    }
+
+    struct Node* temp = front;
+    printf("Queue elements: ");
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    display();
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    display(); 
+    dequeue();
+    display(); 
+    dequeue();
+    dequeue();
+    dequeue();
+    return 0;
+}
 
