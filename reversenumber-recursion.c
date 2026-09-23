490d504bcca1b400b16ec89e5b9711d0e6877db6
unnatissrivastava <unnati.srivastava66@gmail.com>
2026-09-23 20:04:44 +0530

    Update reversenumber-recursion.c

--- reversenumber-recursion.c
+++ reversenumber-recursion.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+int rev = 0;
+void reverse(int n, rev){
+    if(n==0) return;
+    //rev = (rev*10) + (n%10);
+    //reverse(n/10);
+    return reverse(n/10, rev*10+n%10);
+}
+int main(){
+    int n = 321;
+    reverse(n);
+    printf("%d", rev);
+    return 0;
+}
